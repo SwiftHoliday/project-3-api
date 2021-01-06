795d8aedc45ba024f8a786d2ff6fdedaedc23a03
Jonathyn Carter <jonathyncarter@gmail.com>
2021-01-05 22:23:18 -0500

    adds working server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('morgan');
+
+
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+
+app.use(express.json());
+app.use(logger('dev'));
+
+// app.use(cors());
+
+// mount our routes
+//app.use('/api/users', require('./routes/api/users'));
+
+const port = process.env.PORT || 3001;
+// tell express to listen
+app.listen(port, function () {
+    console.log(`Express is listening on port ${port}`);
+});
